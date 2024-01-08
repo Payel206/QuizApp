39652a2e69a5f8a6becc60ddff34ed3a66b15050
Payel Debnath <141117643+Payel206@users.noreply.github.com>
2024-01-08 21:32:12 +0530

    Add files via upload

---
+++ quiz_data.py
@@ -0,0 +1,53 @@
+quiz_data = [
+    {
+        "question": "What is the capital of France?",
+        "choice": ["Paris", "London", "Berlin", "Madrid"],
+        "answer": "Paris"
+    },
+    {
+        "question": "Who wrote Romeo and Juliet?",
+        "choice": ["Charles Dickens", "William Shakespeare", "Jane Austen", "Mark Twain"],
+        "answer": "William Shakespeare"
+    },
+    {
+        "question": "What is the chemical symbol for water?",
+        "choice": ["H2O", "CO2", "O2", "N2"],
+        "answer": "H2O"
+    },
+    {
+        "question": "Which planet is known as the Red Planet?",
+        "choice": ["Earth", "Mars", "Venus", "Jupiter"],
+        "answer": "Mars"
+    },
+    {
+        "question": "What does HTML stand for?",
+        "choice": ["Hyper Text Markup Language", "High-Level Text Markup Language", "Hyperlink and Text Markup Language",
+                    "Home Tool Markup Language"],
+        "answer": "Hyper Text Markup Language"
+    },
+    {
+        "question": "Which programming language is known for its use in machine learning and data science?",
+        "choice": ["Java", "Python", "C++", "Ruby"],
+        "answer": "Python"
+    },
+    {
+        "question": "Who was the first President of the United States?",
+        "choice": ["Thomas Jefferson", "Abraham Lincoln", "George Washington", "John Adams"],
+        "answer": "George Washington"
+    },
+    {
+        "question": "In which year did World War II end?",
+        "choice": ["1945", "1918", "1939", "1941"],
+        "answer": "1945"
+    },
+    {
+        "question": "Which country is known as the Land of the Rising Sun?",
+        "choice": ["China", "Japan", "South Korea", "Thailand"],
+        "answer": "Japan"
+    },
+    {
+        "question": "Which river is the longest in the world?",
+        "choice": ["Amazon", "Nile", "Yangtze", "Mississippi"],
+        "answer": "Nile"
+    }
+]
